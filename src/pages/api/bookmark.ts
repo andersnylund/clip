e9f3f0b7a7bf9fd17a8299cf2fe78729236fcf7e
Anders Nylund <anders@andersnylund.com>
2020-08-31 20:48:20 +0300

    Add bookmark api route

---
+++ src/pages/api/bookmark.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/client'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({ req })
+  if (!session) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+
+  const { name, url, folderId } = req.body
+
+  if (!name) {
+    return res.status(400).json({ message: 'Name is required' })
+  }
+
+  const bookmark = await prisma.bookmark.create({
+    data: {
+      folder: {
+        connect: {
+          id: folderId,
+        },
+      },
+      name,
+      url,
+      user: {
+        connect: {
+          email: session.user.email,
+        },
+      },
+    },
+  })
+
+  return res.status(201).json(bookmark)
+}
