016e1e628801d39d78344d063af8e4d10f0c040e
Anders Nylund <anders@andersnylund.com>
2020-09-05 20:57:49 +0300

    Add email login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,6 +16,17 @@ const options = {
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
+    Providers.Email({
+      server: {
+        auth: {
+          user: process.env.EMAIL_SERVER_USER!,
+          pass: process.env.EMAIL_SERVER_PASSWORD!,
+        },
+        host: process.env.EMAIL_SERVER_HOST!,
+        port: Number(process.env.EMAIL_SERVER_PORT!),
+      },
+      from: process.env.EMAIL_FROM!,
+    }),
   ],
   adapter: Adapters.Prisma.Adapter({ prisma }),
   secret: process.env.NEXT_AUTH_SECRET,
