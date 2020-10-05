b8046c19b3e42d75f26c69eda8ba75afa66c43cf
Anders Nylund <anders@andersnylund.com>
2020-10-05 20:11:39 +0300

    Show clips on public profile (#61)

--- test/components/FolderList.test.tsx
+++ test/components/FolderList.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { FolderList } from '../../src/components/FolderList'
 
 describe('<FolderList />', () => {
-  it('renders', () => {
-    const { container } = render(
+  it('renders a clip', () => {
+    render(
       <FolderList
         folders={[
           {
@@ -15,34 +15,6 @@ describe('<FolderList />', () => {
         ]}
       />
     )
-    expect(container).toMatchInlineSnapshot(`
-      .c0 {
-        display: grid;
-        grid-gap: 1rem;
-        grid-template-columns: repeat(auto-fit,minmax(200px,1fr));
-        list-style-type: none;
-        margin: 2rem 0;
-        padding: 0;
-        width: 100%;
-      }
-
-      .c1 {
-        border-radius: 8px;
-        border: 1px solid lightgrey;
-        padding: 16px;
-      }
-
-      <div>
-        <ul
-          class="c0"
-        >
-          <li
-            class="c1"
-          >
-            folderName
-          </li>
-        </ul>
-      </div>
-    `)
+    expect(screen.getByText('clipName')).toHaveAttribute('href', 'clipUrl')
   })
 })
