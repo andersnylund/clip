37afc1719d1a4dd6de053420a77e153564149bda
Anders Nylund <anders@andersnylund.com>
2020-10-05 20:07:02 +0300

    Show clips on public profile

--- src/components/FolderList.tsx
+++ src/components/FolderList.tsx
@@ -6,7 +6,16 @@ import { Folder } from '../types'
 export const FolderList: FC<{ folders: Folder[] }> = ({ folders }) => (
   <List>
     {folders.map((folder) => (
-      <FolderItem key={folder.id}>{folder.name}</FolderItem>
+      <FolderItem key={folder.id}>
+        <h2>{folder.name}</h2>
+        <ClipList>
+          {folder.clips.map((clip) => (
+            <ClipItem key={clip.id}>
+              <a href={clip.url}>{clip.name}</a>
+            </ClipItem>
+          ))}
+        </ClipList>
+      </FolderItem>
     ))}
   </List>
 )
@@ -17,12 +26,33 @@ const List = styled.ul`
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   list-style-type: none;
   margin: 2rem 0;
+  max-width: 1200px;
   padding: 0;
   width: 100%;
+
+  h2 {
+    margin: 8px 0;
+    padding: 0;
+  }
 `
 
 const FolderItem = styled.li`
   border-radius: 8px;
   border: 1px solid lightgrey;
   padding: 16px;
+`
+
+const ClipList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`
+
+const ClipItem = styled.li`
+  padding: 4px;
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
 `
