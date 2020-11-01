1f705751b99eaa5a92d9d754c5ba5e64bf7c5205
Anders Nylund <anders@andersnylund.com>
2020-11-01 15:13:44 +0200

    Update dependencies

--- test/setup.ts
+++ test/setup.ts
@@ -5,3 +5,11 @@ import jestFetchMock from 'jest-fetch-mock'
 jestFetchMock.enableMocks()
 
 process.env.APP_URL = 'http://localhost:3000'
+process.env.EMAIL_SERVER_HOST = 'testvalue'
+process.env.EMAIL_SERVER_PASSWORD = 'testvalue'
+process.env.EMAIL_SERVER_PORT = '123'
+process.env.EMAIL_SERVER_USER = 'testvalue'
+process.env.GITHUB_ID = 'testvalue'
+process.env.GITHUB_SECRET = 'testvalue'
+process.env.NEXTAUTH_SECRET = 'testvalue'
+process.env.NEXTAUTH_URL = 'testvalue'
