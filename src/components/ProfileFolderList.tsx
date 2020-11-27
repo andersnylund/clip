813e0cbb3453b3b3690e36ad897a6a61a1493e08
Anders Nylund <anders@andersnylund.com>
2020-11-27 17:43:26 +0200

    Refactor clip reordering function

--- src/components/ProfileFolderList.tsx
+++ src/components/ProfileFolderList.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { DragDropContext, DropResult } from 'react-beautiful-dnd'
+import { DragDropContext, DraggableLocation, DropResult } from 'react-beautiful-dnd'
 import styled from 'styled-components'
 import { mutate, cache } from 'swr'
 import { produce } from 'immer'
@@ -8,13 +8,62 @@ import { PROFILE_PATH, useProfile } from '../hooks/useProfile'
 import { Clip, User } from '../types'
 import { ProfileFolder } from './ProfileFolder'
 
+const reorderClip = async (
+  draggableId: string,
+  source: DraggableLocation,
+  destination: DraggableLocation
+): Promise<void> => {
+  const cachedUser: User = cache.get(PROFILE_PATH)
+
+  // TODO: remove istanbul ignore
+  /* istanbul ignore next */
+  const user = produce(cachedUser, (draftUser) => {
+    const folder = draftUser.folders.find((folder) => folder.id === destination.droppableId)
+    const previousFolder = draftUser.folders.find((folder) => folder.id === source.droppableId)
+    const clip = draftUser.folders.reduce((prev: Clip | undefined, curr) => {
+      if (prev) {
+        return prev
+      }
+      return curr.clips.find((clip) => clip.id === draggableId)
+    }, undefined)
+    if (folder && clip) {
+      const newClip = produce(clip, (draftClip) => {
+        draftClip.orderIndex = destination.index
+      })
+      const clips = folder.clips.filter((c) => c.id !== draggableId)
+      clips.splice(destination.index ?? 0, 0, newClip)
+      folder.clips = clips
+      if (previousFolder && source.droppableId !== destination.droppableId) {
+        previousFolder.clips = previousFolder.clips.filter((clip) => clip.id !== draggableId)
+      }
+    }
+  })
+
+  mutate(PROFILE_PATH, user, false)
+
+  await fetch(`/api/clip/${draggableId}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      orderIndex: destination.index,
+      folderId: destination.droppableId,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  mutate(PROFILE_PATH)
+}
+
 export const ProfileFolderList: FC = () => {
   const { profile } = useProfile()
 
   // TODO: remove istanbul ignore
   /* istanbul ignore next */
-  const handleOnDragEnd = async ({ draggableId, type, source, destination }: DropResult): Promise<void> => {
-    if (type !== 'CLIP' || (destination?.droppableId === source?.droppableId && destination.index === source.index)) {
+  const handleOnDragEnd = async (dropResult: DropResult): Promise<void> => {
+    const { draggableId, source, type, destination } = dropResult
+
+    if (destination?.droppableId === source?.droppableId && destination.index === source.index) {
       return
     }
 
@@ -22,44 +71,9 @@ export const ProfileFolderList: FC = () => {
       return
     }
 
-    const cachedUser: User = cache.get(PROFILE_PATH)
-
-    const user = produce(cachedUser, (draftUser) => {
-      const folder = draftUser.folders.find((folder) => folder.id === destination.droppableId)
-      const previousFolder = draftUser.folders.find((folder) => folder.id === source.droppableId)
-      const clip = draftUser.folders.reduce((prev: Clip | undefined, curr) => {
-        if (prev) {
-          return prev
-        }
-        return curr.clips.find((clip) => clip.id === draggableId)
-      }, undefined)
-      if (folder && clip) {
-        const newClip = produce(clip, (draftClip) => {
-          draftClip.orderIndex = destination.index
-        })
-        const clips = folder.clips.filter((c) => c.id !== draggableId)
-        clips.splice(destination.index ?? 0, 0, newClip)
-        folder.clips = clips
-        if (previousFolder && source.droppableId !== destination.droppableId) {
-          previousFolder.clips = previousFolder.clips.filter((clip) => clip.id !== draggableId)
-        }
-      }
-    })
-
-    mutate(PROFILE_PATH, user, false)
-
-    await fetch(`/api/clip/${draggableId}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        orderIndex: destination.index,
-        folderId: destination.droppableId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-
-    mutate(PROFILE_PATH)
+    if (type === 'CLIP') {
+      return reorderClip(draggableId, source, destination)
+    }
   }
 
   return (
