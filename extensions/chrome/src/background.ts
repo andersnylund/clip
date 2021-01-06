296517490c6a5548744991c49a5631ae055b6612
Anders Nylund <anders@andersnylund.com>
2021-01-06 16:09:07 +0200

    Add hot-reload.js

--- extensions/chrome/src/background.ts
+++ extensions/chrome/src/background.ts
@@ -6,6 +6,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   //   }
   // )
 
+  console.log('request', request)
+  console.log('sender', sender)
+
   sendResponse({ message: 'success' })
   return true
 })
