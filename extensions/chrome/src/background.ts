7c68b15f11d569e5b57045a26830066238706de8
Anders Nylund <anders@andersnylund.com>
2021-01-06 16:29:31 +0200

    Implement sending bookmarks to next.js app

--- extensions/chrome/src/background.ts
+++ extensions/chrome/src/background.ts
@@ -1,14 +1,6 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // chrome.bookmarks.create(
-  //   { parentId: bookmarkBarId, title: 'Test bookmark', url: 'https://google.com' },
-  //   function (newFolder) {
-  //     console.log('added folder: ' + newFolder.title)
-  //   }
-  // )
-
-  console.log('request', request)
-  console.log('sender', sender)
-
-  sendResponse({ message: 'success' })
+  chrome.bookmarks.getTree((results) => {
+    sendResponse(results)
+  })
   return true
 })
