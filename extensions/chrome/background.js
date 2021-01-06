df36b37cdc4c8392efa444755557218036e1dcab
Anders Nylund <anders@andersnylund.com>
2021-01-06 09:56:00 +0200

    Start implementation of chrome extension

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -1,1 +1,13 @@
-alert('background running')
+let bookmarkBarId = '1'
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  chrome.bookmarks.create(
+    { parentId: bookmarkBarId, title: 'Test bookmark', url: 'https://google.com' },
+    function (newFolder) {
+      console.log('added folder: ' + newFolder.title)
+    }
+  )
+
+  sendResponse({ message: 'success' })
+  return true
+})
