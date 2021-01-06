e5fcba748d423dad413c42d84df37c73cc91a1cd
Anders Nylund <anders@andersnylund.com>
2021-01-06 20:20:40 +0200

    Add content.ts script to enable background script
    The popup is only running when the popup is opened. However this can't be used listen to messages from the site

--- extensions/chrome/webpack.common.js
+++ extensions/chrome/webpack.common.js
@@ -1,39 +1,40 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   entry: {
-    popup: path.join(__dirname, "src/popup/index.tsx"),
-    eventPage: path.join(__dirname, "src/eventPage.ts")
+    popup: path.join(__dirname, 'src/popup/index.tsx'),
+    eventPage: path.join(__dirname, 'src/eventPage.ts'),
+    content: path.join(__dirname, 'src/content.ts'),
   },
   output: {
-    path: path.join(__dirname, "dist/js"),
-    filename: "[name].js"
+    path: path.join(__dirname, 'dist/js'),
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: 'ts-loader',
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader" // Creates style nodes from JS strings
+            loader: 'style-loader', // Creates style nodes from JS strings
           },
           {
-            loader: "css-loader" // Translates CSS into CommonJS
+            loader: 'css-loader', // Translates CSS into CommonJS
           },
           {
-            loader: "sass-loader" // Compiles Sass to CSS
-          }
-        ]
-      }
-    ]
+            loader: 'sass-loader', // Compiles Sass to CSS
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
-};
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+}
