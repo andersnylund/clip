36c77cd434d94077ec713d28e6de605a53250b0c
Anders Nylund <anders@andersnylund.com>
2021-01-07 20:16:31 +0200

    Update icons

--- extensions/chrome/src/popup/index.tsx
+++ extensions/chrome/src/popup/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Popup from './Popup'
 
-chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
+chrome.tabs.query({ active: true, currentWindow: true }, () => {
   ReactDOM.render(<Popup />, document.getElementById('popup'))
 })
