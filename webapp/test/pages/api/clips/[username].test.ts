b6da91cf3d57f79523f47a38fc78b701e007f023
Anders Nylund <anders@andersnylund.com>
2021-02-12 21:35:03 +0200

    Update coverage

--- webapp/test/pages/api/clips/[username].test.ts
+++ webapp/test/pages/api/clips/[username].test.ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
-
+import { NextApiRequest, NextApiResponse } from 'next'
+import { mocked } from 'ts-jest/utils'
 import handler, { PrismaUserWithClips } from '../../../../src/pages/api/clips/[username]'
 
 jest.mock('@prisma/client')
@@ -16,16 +16,16 @@ const mockUser: PrismaUserWithClips = {
   username: 'username',
   clips: [
     {
-      id: 'folderId',
-      title: 'folderName',
+      id: 'clipId1',
+      title: 'clipTitle1',
       userId: 1,
       parentId: null,
       index: 0,
       url: null,
       clips: [
         {
-          id: 'clipId',
-          title: 'clipName',
+          id: 'clipId2',
+          title: 'clipTitle2',
           url: 'clipUrl',
           index: 0,
           userId: 1,
@@ -70,19 +70,21 @@ describe('username api', () => {
     expect(json).toHaveBeenCalledWith({
       clips: [
         {
-          id: 'folderId',
-          title: 'folderName',
+          id: 'clipId1',
+          title: 'clipTitle1',
           parentId: null,
           index: 0,
           url: null,
+          userId: 1,
           clips: [
             {
-              id: 'clipId',
-              title: 'clipName',
+              id: 'clipId2',
+              title: 'clipTitle2',
               url: 'clipUrl',
               index: 0,
               parentId: null,
               clips: [],
+              userId: 1,
             },
           ],
         },
@@ -135,17 +137,99 @@ describe('username api', () => {
             {
               clips: [],
               parentId: null,
-              id: 'clipId',
-              title: 'clipName',
+              id: 'clipId2',
+              title: 'clipTitle2',
               url: 'clipUrl',
+              userId: 1,
               index: 0,
             },
           ],
-          id: 'folderId',
+          id: 'clipId1',
           index: 0,
           parentId: null,
-          title: 'folderName',
+          title: 'clipTitle1',
           url: null,
+          userId: 1,
+        },
+      ],
+      id: 1,
+      image: 'image',
+      name: 'name',
+      username: 'username',
+    })
+  })
+
+  it('recursively calls the getChildren function', async () => {
+    const findUnique = jest.fn().mockReturnValue(mockUser)
+    const findMany = mocked(PrismaClient.prototype.clip.findMany)
+    findMany.mockResolvedValueOnce([
+      { id: 'clipId2', index: 0, parentId: null, title: 'clipTitle2', url: 'clipUrl2', userId: 1 },
+    ])
+    findMany.mockResolvedValueOnce([
+      { id: 'clipId3', index: 0, parentId: null, title: 'clipTitle3', url: 'clipUrl3', userId: 1 },
+    ])
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    PrismaClient.prototype.user = { findUnique }
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    PrismaClient.prototype.clip = { findMany }
+    const json = jest.fn()
+    const status = jest.fn().mockReturnValue({ json })
+    await handler(
+      ({ query: { username: 'username' } } as unknown) as NextApiRequest,
+      ({ status, json } as unknown) as NextApiResponse
+    )
+    expect(findUnique).toHaveBeenCalledWith({
+      where: {
+        username: 'username',
+      },
+      include: {
+        clips: {
+          where: {
+            parentId: null,
+          },
+          orderBy: {
+            url: 'asc',
+          },
+        },
+      },
+    })
+
+    expect(findMany).toHaveBeenCalledTimes(5)
+    expect(findMany).toHaveBeenNthCalledWith(1, { orderBy: { url: 'asc' }, where: { parentId: 'clipId1' } })
+    expect(findMany).toHaveBeenNthCalledWith(5, { orderBy: { url: 'asc' }, where: { parentId: 'clipId3' } })
+
+    expect(json).toHaveBeenCalledWith({
+      clips: [
+        {
+          id: 'clipId1',
+          title: 'clipTitle1',
+          parentId: null,
+          index: 0,
+          url: null,
+          userId: 1,
+          clips: [
+            {
+              id: 'clipId2',
+              title: 'clipTitle2',
+              url: 'clipUrl2',
+              index: 0,
+              parentId: null,
+              userId: 1,
+              clips: [
+                {
+                  id: 'clipId3',
+                  title: 'clipTitle3',
+                  url: 'clipUrl3',
+                  index: 0,
+                  parentId: null,
+                  userId: 1,
+                  clips: [],
+                },
+              ],
+            },
+          ],
         },
       ],
       id: 1,
