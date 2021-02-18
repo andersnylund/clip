5d6e69eb77cbea876eb661439cfbf70c64e2ce70
Anders Nylund <anders@andersnylund.com>
2021-02-18 22:02:19 +0200

    Update /api/[clipId].ts to use next-connect (#191)

--- webapp/src/pages/api/clips/import.ts
+++ webapp/src/pages/api/clips/import.ts
@@ -1,54 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { User } from 'next-auth'
-import { getSession, Session } from 'next-auth/client'
-import nc, { ErrorHandler, Middleware, RequestHandler } from 'next-connect'
+import nc, { RequestHandler } from 'next-connect'
+import { authorizedRoute, HttpError, onError, onNoMatch, SessionNextApiRequest, SimpleClip } from '../../../api-utils'
 import prisma from '../../../prisma'
-import { Clip } from '../../../types'
 import { getChildren } from './[username]'
-
-export type SimpleClip = Omit<Clip, 'userId'> & {
-  clips: SimpleClip[]
-}
-
-class HttpError extends Error {
-  status?: number = 500
-
-  constructor(message: string, status: number) {
-    super(message)
-    this.message = message
-    this.status = status
-  }
-}
-
-type SessionNextApiRequest = NextApiRequest & {
-  session: Session & {
-    user: User & {
-      email: string
-    }
-  }
-}
-
-const authorizedRoute: Middleware<SessionNextApiRequest, NextApiResponse> = async (req, res, next) => {
-  const session = await getSession({ req })
-  const email = session?.user.email
-  if (!session || !email) {
-    throw new HttpError('Unauthorized', 401)
-  }
-  req.session = {
-    ...session,
-    user: {
-      ...session.user,
-      email,
-    },
-  }
-
-  next()
-}
-
-const onError: ErrorHandler<NextApiRequest, NextApiResponse> = (err: HttpError, req, res) =>
-  // TODO: fix coverage
-  // TODO: fix that prisma errors are shown to user in message
-  res.status(err.status || /* istanbul ignore next */ 500).json({ message: err.message })
 
 const createClip = async (clip: SimpleClip, email?: string, parentId?: string) => {
   const parentData = {
@@ -105,7 +59,7 @@ const post: RequestHandler<SessionNextApiRequest, NextApiResponse> = async (req,
   return res.json(mappedClips)
 }
 
-const handler = nc<NextApiRequest, NextApiResponse>({ onError })
+const handler = nc<NextApiRequest, NextApiResponse>({ onError, onNoMatch })
   .use(authorizedRoute)
   .post(post)
 
--- webapp/test/integration-test-hooks.ts
+++ webapp/test/integration-test-hooks.ts
@@ -1,12 +1,16 @@
 import http from 'http'
-import { NextApiHandler } from 'next'
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+import { NextConnect } from 'next-connect'
 import { apiResolver } from 'next/dist/next-server/server/api-utils'
 import prisma from '../src/prisma'
 import { cleanUp, seed } from './db-test-setup'
 
 let server: http.Server
 
-export const setup = async (handler: NextApiHandler, query?: unknown): Promise<void> => {
+export const setup = async (
+  handler: NextApiHandler | NextConnect<NextApiRequest, NextApiResponse>,
+  query?: unknown
+): Promise<void> => {
   server = http.createServer((req, res) =>
     apiResolver(
       req,
