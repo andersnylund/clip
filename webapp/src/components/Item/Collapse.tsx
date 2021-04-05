07386bf2b74fb85dd8550d8561035e67e3067127
Anders Nylund <anders@andersnylund.com>
2021-04-05 13:19:42 +0300

    Replace Action with tailwind components

---
+++ webapp/src/components/Item/Collapse.tsx
@@ -0,0 +1,13 @@
+import { ChevronRightIcon } from '@radix-ui/react-icons'
+import React, { FC } from 'react'
+
+interface Props {
+  onClick: () => void
+  collapsed?: boolean
+}
+
+export const Collapse: FC<Props> = ({ onClick, collapsed }) => (
+  <button onClick={onClick} className={`p-2 transform transition-transform ${!collapsed ? 'rotate-90' : ''}`}>
+    <ChevronRightIcon />
+  </button>
+)
--- webapp/src/components/Item/Handle.tsx
+++ webapp/src/components/Item/Handle.tsx
@@ -1,10 +1,8 @@
+import { DragHandleDots2Icon } from '@radix-ui/react-icons'
 import React, { FC } from 'react'
-import { Action, Props } from './Action'
 
-export const Handle: FC<Props> = (props) => (
-  <Action data-cypress="draggable-handle" {...props}>
-    <svg viewBox="0 0 20 20" width="12">
-      <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"></path>
-    </svg>
-  </Action>
+export const Handle: FC<React.HTMLAttributes<HTMLButtonElement>> = (props) => (
+  <button {...props} className="cursor-move p-2 m-2">
+    <DragHandleDots2Icon />
+  </button>
 )
