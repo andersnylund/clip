7b3436378248be3a40c1fe77887bbb2c6b5f5a4c
Anders Nylund <anders@andersnylund.com>
2021-05-15 12:06:41 +0300

    Fix bug separators not filtered out recursively

--- extension/src/background/filter.ts
+++ extension/src/background/filter.ts
@@ -1,9 +1,21 @@
 import { Bookmarks } from 'webextension-polyfill-ts'
 
-export const filterBookmark = (bookmark?: Bookmarks.BookmarkTreeNode): Bookmarks.BookmarkTreeNode | undefined =>
-  bookmark
-    ? {
-        ...bookmark,
-        children: bookmark.children?.filter((child) => child.type !== 'separator'),
-      }
-    : bookmark
+/**
+ * Filters out separators from the complete tree of bookmarks. Separators are a firefox only feature
+ * @param bookmark The bookmark which children to filter recursively
+ * @returns a bookmark without separators as children
+ */
+export const filterBookmark = (bookmark?: Bookmarks.BookmarkTreeNode): Bookmarks.BookmarkTreeNode | undefined => {
+  if (bookmark?.children) {
+    const asdf = bookmark.children
+      .filter((child) => child.type !== 'separator')
+      .map(filterBookmark)
+      .filter((child?: Bookmarks.BookmarkTreeNode): child is Bookmarks.BookmarkTreeNode => child !== undefined)
+    return {
+      ...bookmark,
+      children: asdf,
+    }
+  } else {
+    return bookmark
+  }
+}
