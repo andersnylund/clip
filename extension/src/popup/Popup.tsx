cc569899950416c8b251a1c90036ca6966a250c7
Anders Nylund <anders@andersnylund.com>
2021-07-03 21:36:23 +0300

    Fix login name (#359)
    * Fix login name
    
    * Update extension popup tests

--- extension/src/popup/Popup.tsx
+++ extension/src/popup/Popup.tsx
@@ -3,23 +3,25 @@ import { getAppUrl } from '../../../shared/app-url'
 import { useProfile } from '../../../shared/hooks/useProfile'
 
 const Popup: FC = () => {
-  const { profile } = useProfile()
+  const { profile, isLoading } = useProfile()
 
   return (
     <div className="p-12 flex flex-col gap-4 justify-center items-center">
       <h1 className="text-2xl">clip.so</h1>
-      <div>
-        {profile ? (
-          <span>`Logged in as ${profile.name}`</span>
-        ) : (
-          <span>
-            Not logged in. Log in at{' '}
-            <a className="text-black underline" href={getAppUrl()}>
-              {getAppUrl()}
-            </a>
-          </span>
-        )}
-      </div>
+      {!isLoading && (
+        <div>
+          {profile ? (
+            <span>Logged in as {profile.name}</span>
+          ) : (
+            <span>
+              Not logged in. Log in at{' '}
+              <a className="text-black underline" href={getAppUrl()}>
+                {getAppUrl()}
+              </a>
+            </span>
+          )}
+        </div>
+      )}
     </div>
   )
 }
--- extension/test/setup.ts
+++ extension/test/setup.ts
@@ -1,1 +1,3 @@
+import '@testing-library/jest-dom/extend-expect'
+
 process.env.APP_URL = 'http://localhost:3001'
